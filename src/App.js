a5cabd5dbedd6163fe12988f8ba70c435cb4a9fa
Aldo <aldovallerauy@gmail.com>
2022-09-23 18:59:29 -0300

    atc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import React from 'react';
 
 //Importamos nuestros commponentes
 import NavBarIni from "./components/NavBar/NavBar";
@@ -7,29 +7,26 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
 import { CartProvider } from './components/context/CartContext';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <CartProvider>
-      <div className="App">
-      
-        <BrowserRouter>
-          <NavBarIni/>
-          <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="/categorias/:id" element={<ItemListContainer />} />
-            <Route path="/item/:id" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart />} />
-
-            <Route path="/*" element={<Navigate replace to="/" />} />
-          </Routes>
-        </BrowserRouter>
-
-      </div> 
+      <BrowserRouter>
+        <div className="App">
+          <div>
+            <NavBarIni/>
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route path="/categorias/:id" element={<ItemListContainer />} />
+              <Route path="/item/:id" element={<ItemDetailContainer />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/*" element={<Navigate replace to="/" />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </CartProvider>
-      
-
-
   );
 }
 
